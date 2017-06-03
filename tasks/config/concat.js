19115894d00ba92096e901240cc700d444363fa3
Bradley Bain <brad_bain@live.com>
2017-06-03 12:43:44 -0500

    (Temporarily) disable uglify production grunt task

--- tasks/config/concat.js
+++ tasks/config/concat.js
@@ -19,7 +19,8 @@ module.exports = function(grunt) {
   grunt.config.set('concat', {
     js: {
       src: require('../pipeline').jsFilesToInject,
-      dest: '.tmp/public/concat/production.js'
+      //dest: '.tmp/public/concat/production.js'
+      dest: '.tmp/public/min/production.min.js'
     },
     css: {
       src: require('../pipeline').cssFilesToInject,
